ea9f8c7cf01b6ec4325fa8de249c0e4f5ff0fa94
Shubham Baranwal <93540042+Sbaranwal9@users.noreply.github.com>
2021-11-21 16:11:51 +0000

    canSum changes

--- canSum.cpp
+++ canSum.cpp
@@ -3,16 +3,20 @@
 #include <unordered_map>
 
 bool canSum(int n, std::vector<int> a, std::unordered_map<int, bool> &m){
+  if(m.find(n) != m.end()) return m[n];
   if(n == 0) return true;
   if(n < 0) return false;
   for(auto i : a){
-    if(m.find(n-i) == m.end()) m[n-i] = canSum(n-i, a, m);
-    if(m[n-i]) return true;
+    if(canSum(n-i, a, m)){
+      m[n] = true;
+      return true;
+    }
   }
+  m[n] = false;
   return false;
 }
 
 int main(){
   std::unordered_map<int, bool> m;
-  std::cout << canSum(300, {7, 14}, m) << std::endl;
+  std::cout << canSum(300, {7, 15}, m) << std::endl;
 }
