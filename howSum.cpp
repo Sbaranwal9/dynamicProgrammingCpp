68315e91d0c7b650e3dd49c98c5ceb8e78f9824b
Shubham Baranwal <93540042+Sbaranwal9@users.noreply.github.com>
2021-11-21 16:19:41 +0000

    howSum added

--- howSum.cpp
+++ howSum.cpp
@@ -3,16 +3,16 @@
 #include <unordered_map>
 
 std::vector<int> howSum(int n, std::vector<int> v, std::unordered_map<int, std::vector<int>> &m){
+  if(m.find(n) != m.end()) return m[n];
   if(n == 0) return {};
   if(n < 0) return {-1};
   for(auto i : v){
     int r = n - i;
-    if(m.find(r) == m.end()){
-      m[r] = howSum(r, v, m);
-    }
-    if(m[r].empty() || (!m[r].empty() && m[r][0] != -1)) {
-      m[r].push_back(i);
-      return m[r];
+    std::vector<int> a = howSum(r, v, m);
+    if(a.empty() || (!a.empty() && a[0] != -1)) {
+      a.push_back(i);
+      m[n] = a;
+      return m[n];
     }
   }
   return {-1};  
