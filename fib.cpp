13f8ffbfc46f39804f64bdbd52396702a7e6172a
Shubham Baranwal <93540042+Sbaranwal9@users.noreply.github.com>
2022-01-07 03:58:11 +0000

    fib tabulation

--- fib.cpp
+++ fib.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <unordered_map>
 
 int fib(int n, std::unordered_map<int, int> &m){
@@ -8,9 +9,19 @@ int fib(int n, std::unordered_map<int, int> &m){
   return m[n];
 }
 
+int fibTab(int n){
+  std::vector<int> a(n+1, 0);
+  a[1] = 1;
+  for(int i=2; i <= n; i++){
+    a[i] = a[i-1] + a[i-2];
+  }
+  return a[n];
+}
+
 int main() {
   int n;
   std::cin >> n;
   std::unordered_map<int, int> m;
-  std::cout << fib(n, m) << std::endl;
+  // std::cout << fib(n, m) << std::endl;
+  std::cout << fibTab(n) << std::endl;
 } 
