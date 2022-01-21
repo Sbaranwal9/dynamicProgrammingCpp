141e9c189ccb255777e1cf7e482cee34f2519eef
Shubham Baranwal <93540042+Sbaranwal9@users.noreply.github.com>
2022-01-21 20:50:02 +0000

    best sum tab

--- bestSum.cpp
+++ bestSum.cpp
@@ -21,9 +21,29 @@ std::vector<int> bestSum(int n, std::vector<int> v, std::unordered_map<int, std:
   return ar;
 }
 
+std::vector<int> bestSumTab(int target, std::vector<int> numbers){
+  std::vector<std::vector<int>> a(target+1, std::vector<int> (1,-1));
+  a[0] = {};
+  for(int i = 0; i <= target; i++){
+    if(a[i].empty() || (!a[i].empty() && a[i][0] != -1)){
+      for(int j : numbers){
+        if(i + j <= target){
+          std::vector<int> c(a[i]);
+          c.push_back(j);
+          if((!a[i + j].empty() && a[i + j][0] == -1) || c.size() < a[i + j].size()){
+            a[i + j] = c;
+          }
+        }
+      }
+    }
+  }
+  return a[target]; 
+}
+
 int main(){
-  std::unordered_map<int, std::vector<int>> m;
-  std::vector<int> a = bestSum(100, {1, 2, 5, 25}, m);
+  // std::unordered_map<int, std::vector<int>> m;
+  // std::vector<int> a = bestSum(100, {1, 2, 5, 25}, m);
+  std::vector<int> a = bestSumTab(100, {1, 2, 5, 25});
   for(auto i : a){
     std::cout << i << " ";
   }
