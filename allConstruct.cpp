1dcb57d2bda723716a35a2e4c6f57535e4c7de64
Shubham Baranwal <93540042+Sbaranwal9@users.noreply.github.com>
2022-01-26 21:30:19 +0000

    allConstructTab added

--- allConstruct.cpp
+++ allConstruct.cpp
@@ -20,9 +20,32 @@ std::vector<std::vector<std::string>> allConstruct(std::string target, std::vect
   return v;
 }
 
+std::vector<std::vector<std::string>> allConstructTab(std::string target, std::vector<std::string> wordBank){
+  std::vector<std::vector<std::vector<std::string>>> a(target.length() + 1);
+  std::vector<int> check(target.length() + 1, 0);
+  check[0] = 1;
+  a[0] = {{{""}}};
+  for(int i = 0; i <= target.length(); i++){
+    if(check[i] != 0){
+      for(auto word : wordBank){
+        if(i + word.length() <= target.length() && target.substr(i, word.length()).compare(word) == 0){
+          check[i + word.length()] = 1;
+          std::vector<std::vector<std::string>> c = a[i];
+          for(auto j : c){
+            j.push_back(word);
+            a[i + word.length()].push_back(j);
+          }
+        }
+      }
+    }
+  }
+  return a[target.size()];
+}
+
 int main(){ 
   std::unordered_map<std::string, std::vector<std::vector<std::string>>> m;
   std::vector<std::vector<std::string>> v = allConstruct("purple", {"purp", "p", "ur", "le", "purpl"}, m);
+  // std::vector<std::vector<std::string>> v = allConstructTab("purple", {"purp", "p", "ur", "le", "purpl"});
   for(auto ar : v){
     for(auto a : ar){
       std::cout << a << " ";
